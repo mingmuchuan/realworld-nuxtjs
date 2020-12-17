8b6e9cc15713a78bf62aa025446b47d447ddb589
mingmuchuan <1871998048@qq.com>
2020-12-17 21:27:28 +0800

    actions 4.0

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-12-14 10:30:09
- * @LastEditTime: 2020-12-16 21:09:59
+ * @LastEditTime: 2020-12-17 21:27:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /realworld-nuxtjs/pages/home/index.vue
@@ -11,7 +11,7 @@
 
         <div class="banner">
           <div class="container">
-            <h1 class="logo-font">github actions3.0</h1>
+            <h1 class="logo-font">github actions4.0</h1>
             <p>A place to share your knowledge.</p>
           </div>
         </div>
